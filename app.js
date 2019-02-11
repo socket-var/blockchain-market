884e8da716ae009f11dd8f1a1e5972e35aaceadf
saketh varma <sakethbobby@gmail.com>
2019-02-11 14:22:00 -0500

    Add code for SPA

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+app.get("*", function(req, res, next) {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
