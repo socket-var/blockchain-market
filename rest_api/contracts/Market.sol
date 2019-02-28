760d7efbd955dbb8d865e15368bf3528496d6bdd
saketh varma <sakethbobby@gmail.com>
2019-02-27 22:47:46 -0500

    Add transaction hash

--- rest_api/contracts/Market.sol
+++ rest_api/contracts/Market.sol
@@ -1,36 +1,20 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.5.0;
 
-// erc standard 
-// totalSupply
-// balanceOf
-// transfer
-// transferFrom
-// approve
-// allowance
-
-
-// constructor initializes owner variable as chairperson
-// register function can be called to register new user
-// on registration 100 tokens would be assigned to each user
-// unregister function would remove the user and transfer remaining tokens to admin
-
-contract Token {
-
-}
-
-contract Market is Token {
-
-    address chairperson;
-    mapping(address => User) users;
-    address[] userArray;
-    uint numTokens = 0;
+contract Market {
 
     struct User {
         bool isRegistered;
         string userType;
         uint balance;
     }
-    
+
+    address private chairperson;
+    mapping(address => User) private users;
+
+    uint private numTokens;
+    uint private nonce = 0;
+
+     
     // modifiers
     modifier onlyChairperson() {
         require(msg.sender == chairperson, "Only chairperson can call this");
@@ -47,12 +31,12 @@ contract Market is Token {
     modifier onlyBuyer() {
         bytes memory buyer = bytes("buyer");
         bytes memory userType = bytes(users[msg.sender].userType);
-        require(keccak256(userType) == keccak256(buyer), "Only seller can call this");
+        require(keccak256(userType) == keccak256(buyer), "Only buyer can call this");
         _;
     }
 
-    modifier onlySelf(address userAddress) {
-        require(msg.sender == userAddress || msg.sender == chairperson, "Only chairperson or the target user can call this");
+    modifier onlyChairpersonOrSelf(address userAddress) {
+        require(msg.sender == chairperson || msg.sender == userAddress, "Only chairperson or the target user can call this");
         _;
     }
 
@@ -71,8 +55,6 @@ contract Market is Token {
         chairperson = msg.sender;
         users[chairperson].userType = "chairperson";
         users[chairperson].isRegistered = true;
-        users[chairperson].balance = 0;
-        userArray.push(chairperson);
     }
     
     /* Chairperson functions go here */
@@ -81,14 +63,8 @@ contract Market is Token {
         return numTokens;
     }
 
-    function balanceOf(address userAddress) public view onlyChairperson onlySelf(userAddress) returns(uint){
+    function balanceOf(address userAddress) public view onlyChairpersonOrSelf(userAddress) returns(uint) {
         return users[userAddress].balance;
-    }
-
-    // will add money to user's wallet once registered
-    function addDeposit(address newUser, uint amount) internal onlyChairperson {
-        users[newUser].balance = amount;
-        emit DepositSuccess(newUser, amount);
     }
 
     /* add more tokens into the system - can only be called by the owner*/
@@ -97,41 +73,54 @@ contract Market is Token {
         emit TokensAdded(amount);
     }
 
+    function addDeposit(address newUser, uint amount) internal {
+        require(numTokens - amount >= 0, "Insufficient tokens. Need to generate more for registering new users");
+        users[newUser].balance = amount; 
+    }
+
     function register(address newUser, string memory userType, uint amount) public onlyChairperson {
         require(users[newUser].isRegistered == false, "already registered user");
         
         users[newUser].userType = userType;
         users[newUser].isRegistered = true;
-        userArray.push(newUser);
+        
+        
         addDeposit(newUser, amount);
+        
+        emit DepositSuccess(newUser, amount);
         emit UserRegistered(newUser);
     }
     
     function unregister(address userAddress) public onlyChairperson{
+        require(users[userAddress].isRegistered == true, "Error: User to unregister not found");
+
         delete users[userAddress];
+        // revokeTokens(userAddress);
         emit UserUnregistered(userAddress);
     }
 
     /* Chairperson functions end here */
 
-    /* Transaction functions start here */
+    /* Buy function */
 
-    function transfer(uint priceInTokens, address to) public {
-        require(users[msg.sender].balance >= priceInTokens, "Transfer failed due to insufficient balance");
-        require(msg.sender != to, "Invalid: Can't transfer token to the same account");
-        users[msg.sender].balance -= priceInTokens;
-        users[to].balance += priceInTokens;
-        emit TransferSuccess(msg.sender, to, priceInTokens);
+    function settlePayment(address buyer, address seller, uint priceInTokens) internal {
+        users[buyer].balance -= priceInTokens;
+        users[seller].balance += priceInTokens;
+        emit TransferSuccess(buyer, seller, priceInTokens);
     }
 
-    function buy(uint priceInTokens, address seller) public onlyBuyer {
-      ///
-        transfer(priceInTokens, seller);
-        emit TransferSuccess(msg.sender, seller, priceInTokens);
-    }
-    
-    function listUsers() public view onlyChairperson returns(address[] memory)  {
-        return userArray;
+    function buy(string memory productId, uint priceInTokens, address seller) public onlyBuyer returns(bytes32) {
+
+        require(msg.sender.balance >= priceInTokens, "Transfer failed due to insufficient balance");
+        require(msg.sender != seller, "Invalid: Can't transfer token to the same account");
+        
+        settlePayment(msg.sender, seller, priceInTokens);
+
+        bytes memory ret = abi.encodePacked(nonce, productId, msg.sender, seller, priceInTokens);
+        
+        nonce += 1;
+
+        return keccak256(ret);
     }
 
 }
